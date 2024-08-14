9e3e8d193c43d3df5983b63e612154c193af2750
LEEDD02 <dangbestrv2002@gmail.com>
2024-08-14 21:57:46 +0700

    fix: connect to databases

--- danglh/login-form/model/connect.php
+++ danglh/login-form/model/connect.php
@@ -1,61 +1,9 @@
 <?php
-function pdo_get_connection(){
-  $servername = '127.0.0.1';
-  $username = "danglh";
-  $password = "password";
+$host = '127.0.0.1';
+$username = "danglh";
+$password = "password";
 
-  $conn = new PDO("mysql:host=$servername;dbname=demo_db", $username, $password);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// chuyen ve su dung mysqli_connect();
 
-}
-
-
-// function pdo_execute($sql) { //function thuc thi
-//   $sql_args = array_slice(func_get_arg(),1);
-//   try {
-//     $conn = pdo_get_connection();
-//     $stmt = $conn->prepare($sql);
-//     $stmt->execute($sql_args);
-//   }
-//   catch(PDOException $e){
-//     throw $e;
-//   }
-//   finally{
-//     unset($conn);
-//   }
-// };
-
-
-// function pdo_query($sql){  // Lay tat ca
-//   $sql_args = array_slice(func_get_arg(),1);
-//   try {
-//     $conn = pdo_get_connection();
-//     $stmt = $conn->prepare($sql);
-//     $stmt->execute($sql_args);
-//     $rows = $stmt->fetchAll();
-//     return $rows;
-//   }
-//   catch(PDOException $e){
-//     throw $e;
-//   }
-//   finally{
-//     unset($conn);
-//   }
-// }
-
-// function pdo_query_one($sql){  //Lay mot
-//   $sql_args = array_slice(func_get_arg(),1);
-//   try {
-//     $conn = pdo_get_connection();
-//     $stmt = $conn->prepare($sql);
-//     $stmt->execute($sql_args);
-//     $row = $stmt->fetch(PDO::FETCH_ASSOC);
-//     return $row;
-//   }
-//   catch(PDOException $e){
-//     throw $e;
-//   }
-//   finally{
-//     unset($conn);
-//   }
-// }
+$conn = new PDO("mysql:host=$host;dbname=demo_db", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- danglh/login-form/process_register.php
+++ danglh/login-form/process_register.php
@@ -1,19 +1,20 @@
 <?php
 require_once 'model/connect.php';
 
-if(isset($_POST['btn-submit'])) {
-  echo '<pre>';
-  print_r($_POST);
-  $fullname = $_POST['fullname'];
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-  $email = $_POST['email'];
-  $sql= "INSERT INTO 'user' ('fullname', 'username', 'password', 'email')
-VALUES ('$fullname', '$username', '$password', '$email')";
-echo $sql;
-if(pdo_get_connection()>$conn->query($sql)===true) {
-  echo ' luu du lieu thanh cong';}
-  else {
-    echo 'loi {$sql}'.$conn->error;
+try {
+  if (isset($_POST['btn-submit'])) {
+    echo '<pre>';
+    print_r($_POST);
+    $fullname = $_POST['fullname'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+    $sql = "INSERT INTO user (fullname, username, password, email) VALUES ('$fullname', '$username', '$password', '$email')";
+    
+    echo $sql;
+
+    $conn->exec($sql);
   }
+} catch(PDOException $e) {
+  echo $e->getMessage();
 }
