16ebeb129b0e39b62b363c1bbea368d7460d021f
LEEDD02 <dangbestrv2002@gmail.com>
2024-08-16 00:13:00 +0700

    feat: building sale-web

---
+++ danglh/login-form/sale_web/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/main.css">
+  <title>Sale_Web</title>
+</head>
+
+<body>
+  <header>
+    <div class="logo">
+
+    </div>
+    <div class="search-container">
+      <form action="/search">
+        <input class="search_text" type="text" placeholder="Search.." name="search">
+        <button class="search_submit" type="search_submit">Search</button>
+      </form>
+    </div>
+    <div class="information">
+      <ul>
+        <li><a href="home.php">HOME</a></li>
+        <div class="line"></div>
+        <li><a href="pro.php">ABOUT</a></li>
+        <div class="line"></div>
+        <li><a href="pro.php">NEWS</a></li>
+        <div class="line"></div>
+        <li><a href="vocabulary.php">LOGIN</a></li>
+      </ul>
+    </div>
+
+  </header>
+  <nav>
+    <ul>
+      <li>
+        <div class="under-line">
+        </div>
+        <div class="category">
+          <a href="home.php">MOBILEPHONE</a>
+        </div>
+      </li>
+
+      <li>
+        <div class="under-line">
+        </div>
+        <div class="category">
+          <a href="pro.php">TABLET</a>
+        </div>
+      </li>
+
+      <li>
+        <div class="under-line">
+        </div>
+        <div class="category">
+          <a href="pro.php">LATOP</a>
+        </div>
+      </li>
+
+      <li>
+        <div class="under-line">
+        </div>
+        <div class="category">
+          <a href="vocabulary.php">TELEVISION</a>
+        </div>
+      </li>
+
+      <li>
+        <div class="under-line">
+        </div>
+        <div class="category">
+          <a href="vocabulary.php">ACCESSORY</a>
+        </div>
+      </li>
+
+      <li>
+        <div class="under-line">
+        </div>
+        <div class="category">
+          <a href="vocabulary.php">WATCH</a>
+        </div>
+      </li>
+
+      <li>
+        <div class="under-line">
+        </div>
+        <div class="category">
+          <a href="vocabulary.php">HEADPHONE</a>
+        </div>
+      </li>
+    </ul>
+  </nav>
+  <section class="banner">
+    <div class="banner_slider_nav">
+      <a href="#slide-1"></a>
+      <a href="#slide-2"></a>
+      <a href="#slide-3"></a>
+    </div>
+    <div class="banner_slider">
+      <div id="slide-1" class="banner_img_container">
+        <img src="img/banner-1.jpg" alt="pic1">
+      </div>
+      <div id="slide-2" class="banner_img_container">
+        <img src="img/banner-2.jpg" alt="pic1">
+      </div>
+      <div id="slide-3" class="banner_img_container">
+        <img src="img/banner-3.jpg" alt="pic1">
+      </div>
+    </div>
+  </section>
+
+</body>
+
+</html>
